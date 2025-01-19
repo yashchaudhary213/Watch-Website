5e3356dce7dc2fe1a4aef672b4e3dd19c31737c8
yashchaudhary213 <yash.technical200@gmail.com>
2025-01-19 19:15:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+const hamburger = document.getElementById("hamburger");
+const navlinks = document.getElementById("navlinks");
+
+hamburger.addEventListener("click", ()=>{
+
+    navlinks.classList.toggle("navlinks-active");
+})
